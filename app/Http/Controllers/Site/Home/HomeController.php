999e6535052fb536886ca675f098595c3f2ead46
FahathAMM <fahathammex90@gmail.com>
2025-04-02 20:44:59 +0400

    Update HomeController.php

--- app/Http/Controllers/Site/Home/HomeController.php
+++ app/Http/Controllers/Site/Home/HomeController.php
@@ -40,6 +40,7 @@ class HomeController extends Controller
         return view('site.home.index', [
             'categories' => $categories,
             'products' => $products,
+            'title' => $this->modelName,
         ]);
     }
 }
