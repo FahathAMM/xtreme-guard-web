b0e82614e7e898e210e92a3a751e83c826cade2c
FahathAmm <fahathammex90@gmail.com>
2025-04-05 14:32:46 +0400

    Update index.blade.php

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -4,7 +4,7 @@
 
     <link rel="stylesheet" href="{{ asset('assets/libs/jsvectormap/css/jsvectormap.min.css') }}">
 
-    {{-- <x-dashboard.dashboard :data="$data" :orders="$orders" :lowStock="$lowStock" :userLogs="$userLogs" /> --}}
+    <x-dashboard.dashboard :data="$data" :orders="$orders" :lowStock="$lowStock" :userLogs="$userLogs" />
 
     {{-- <x-mail.customer-invoice-mail :order="$order" /> --}}
 
