ef5f212b2e51ef541cd88ebe3dc084d938287938
FahathAmm <fahathammex90@gmail.com>
2025-04-05 14:31:20 +0400

    Update HomeController.php

--- app/Http/Controllers/Site/Home/HomeController.php
+++ app/Http/Controllers/Site/Home/HomeController.php
@@ -29,7 +29,7 @@ class HomeController extends Controller
 
     public function index(Request $request)
     {
-        $products = Product::take(9)->with('category')->get();
+        $products = Product::orderBy('created_at', 'desc')->take(9)->with('category')->get();
         $categories = getParentCategories();
 
         // return $categories;
