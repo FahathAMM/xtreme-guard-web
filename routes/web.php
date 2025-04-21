5457781bfab5e9ab0f2d8dbe5f0fa61acb8bc109
fahathAMM <fahathammex90@gmail.com>
2025-04-22 00:10:13 +0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -53,6 +53,7 @@ Route::prefix('admin')->middleware(['auth', 'logged.session'])->group(function (
     Route::resource('solution', SolutionController::class);
     Route::get('solution/delete-img/{id}', [SolutionController::class, 'deleteSolutionImg']);
     Route::get('solution/delete-file/{id}/{key}', [SolutionController::class, 'deleteSolutionFile']);
+    Route::get('solution/delete-img/{path}/{img}/{solution}', [SolutionController::class, 'deleteSolutionImg']);
 
 
     Route::resource('products', ProductController::class);
